8bbc2a1a865a20c9be844da35c17e4b7632d4454
Amit Prasad <amitprasad531@gmail.com>
2019-08-28 23:47:45 +0200

    Added shinywidget for selecting countries and changed functionality for null values

--- server.R
+++ server.R
@@ -1,29 +1,23 @@
 shinyServer(function(input, output, session) {
   
   temp_data <- reactive({
-    req(input$region_data, input$country_data)
     gpw %>%
       filter(Region %in% input$region_data)
   })  
   
   observe({
-    req(input$country_data, input$region_data)
     now <- as.character(input$country_data)
-    updateSelectizeInput(session,
-                         "country_data", "Select Countries",
-                         choices = c("All", unique(as.character(temp_data()$Country))),
-                         selected = now)
+    updatePickerInput(session,
+                         "country_data", "Select Countries (from dropdown list)",
+                         choices = c(unique(as.character(temp_data()$Country))),
+                         selected = input$country_data)
   })
   
   dataTable <- reactive({
-    req(input$country_data)
     if ("All" %in% input$country_data) {
       temp_data() %>%
         filter(Year >= input$year_data[1] & Year <= input$year_data[2]) %>%
         select(Country, Region, Year, input$indicators_data)
-    } else if (is.null(input$indicators_data)) {
-      temp_data() %>%
-        select(Country, Region, Year)
     } else {
       temp_data() %>%
         filter(Country %in% input$country_data) %>%
