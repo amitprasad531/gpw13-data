5c0c89448c2a84227ce4a4117eb71b9bc302d813
Amit Prasad <amitprasad531@gmail.com>
2019-08-29 02:13:13 +0200

    Added functionality for latest available data and updated UI

--- global.R
+++ global.R
@@ -11,3 +11,14 @@ source("www/metadata_data.R")
 for (i in 1:length(gpw_ind)) {
   names(gpw)[names(gpw) == gpw_ind[i]] <- gpw_colnames[i]
 }
+
+map_latest <- function(a, b, yr = "None", value = NA) {
+  for (i in 1:length(a)) {
+    ifelse(is.na(b[i]), value, value <- b[i])
+    if (!is.na(value)) {
+      yr <- a[i]
+      return(c(yr, value))
+    }
+  }
+  return(c(yr, value))
+}
--- server.R
+++ server.R
@@ -14,10 +14,30 @@ shinyServer(function(input, output, session) {
   })
   
   dataTable <- reactive({
-    if ("All" %in% input$country_data) {
-      temp_data() %>%
-        filter(Year >= input$year_data[1] & Year <= input$year_data[2]) %>%
-        select(Country, Region, Year, input$indicators_data)
+    if (input$latest == TRUE) {
+      req(input$country_data, input$indicators_data)
+      df <- temp_data() %>% 
+              select(Country, Year, Region, input$indicators_data) %>%
+              filter(Country %in% input$country_data)
+      temp <- setNames(data.frame(matrix(ncol = 5, nrow = 0)), c("Country", "Region", "Indicator", "Year", "Value"))
+      temp[1, ] <- c("Dummy", "DMY", "Indicator DMY", "2000", "100")
+      for (i in 1:length(unique(df$Country))) {
+        df_temp <- df %>% filter(Country == unique(df$Country)[i])
+        a <- as.character(unique(df_temp$Year))
+        for (j in 1:length(unique(input$indicators_data))) {
+          b <- df_temp[[input$indicators_data[j]]]
+          c <- map_latest(a, b)
+          temp2 <- data.frame("Country" = df_temp$Country[1], 
+                              "Region" = df_temp$Region[1],
+                              "Indicator" = input$indicators_data[j],
+                              "Year" = c[1],
+                              "Value" = c[2])
+          temp <- rbind(temp, temp2)
+        }
+      }
+      num_rows <- nrow(temp)
+      df <- temp[2:num_rows,]
+      # whs_map[[im()]] <- as.numeric(whs_map[[im()]])
     } else {
       temp_data() %>%
         filter(Country %in% input$country_data) %>%
